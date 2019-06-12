71325c26e2558befee559d486a88a883ba1710c8
Anaisofia <anaisrivas.26@gmail.com>
2019-06-11 20:35:43 -0400

    se generó el formulario con sus validadores, se creó la vista done, ruta y método, falta aplicar el descuento y el impuesto (si aplica) y listar los resultados en una tabla en la vista done

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,31 @@
 class SalesController < ApplicationController
   def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+      if @sale.tax == 1
+        taxes = @sale.value - (@sale.value * 19/100)
+        Sale.new(sale_params)
+      else
+        @sale.value - @sale.discount
+      end
+
+
+    if @sale.save
+      redirect_to sales_done_path
+    else
+      render :new
+    end
+  end
+
+  def done
+    @sale = Sale.last
+  end
+
+  private
+  def sale_params
+    params.require(:sale).permit(:cod, :detail, :category, :value, :discount, :tax, :total)
   end
 end
